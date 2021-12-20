1d6592ae02aa4a652168ab1676d443662471971b
konstantin <makarovestates@gmail.com>
2021-12-19 23:05:27 -0600

    First attempt

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2 
+
+let random = Math.floor((Math.random() * 10) + 5)
+
+let a = 17 
+let b = 13
+let mod = a % b
+
+let max = Math.max(18, 19, 20) 
 
 // Write your code here
